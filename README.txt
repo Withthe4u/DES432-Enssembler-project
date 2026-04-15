7be5c4b195067fc87c9c777b8ffbab87a066db62
Withthe4u <phuwaritkunlapan80@gmail.com>
2026-04-15 23:15:24 +0700

    simplifyDataProcessing-Phu

---
+++ README.txt
@@ -0,0 +1,10 @@
+-To use processing.py do
+ 
+    "from processing import allData and get_candidate_movies"
+ 
+Other functions are not needed
+
+-how to use allData()
+
+    data = allData()
+    movies = data["movies"]
--- processing.py
+++ processing.py
@@ -1,52 +1,74 @@
 import pandas as pd
 
-# load all files(MOVIES.DAT HAVE NOT BEEN USED YET)
-train = pd.read_csv("train.csv")
-val = pd.read_csv("val.csv")
-movies = pd.read_csv(
-    "movies.dat",
-    sep="::",
-    engine="python",
-    names=["movie_id", "title", "genres"],
-    encoding="latin-1"
-)
-users = pd.read_csv(
-    "users.dat",
-    sep="::",
-    engine="python",
-    names=["user_id", "gender", "age", "occupation", "zip"],
-    encoding="latin-1"
-)
+def cleanData():
+    
+    # load all files(MOVIES.DAT HAVE NOT BEEN USED YET. WILL NEED WHEN WE WANT TO USE ONE HOT ENCODED)
+    train = pd.read_csv("train.csv")
+    val = pd.read_csv("val.csv")
 
-#Drop "timestamp" because it is unnecessary
-train = train.drop(columns=["timestamp"])
-val = val.drop(columns=["timestamp"])
+    movies = pd.read_csv(
+        "movies.dat",
+        sep="::",
+        engine="python",
+        names=["movie_id", "title", "genres"],
+        encoding="latin-1"
+    )
 
-#Drop empty and duplicates(training set only) dropping duplicates on val is not necessary
-train = train.dropna().drop_duplicates()
-val = val.dropna()
+    users = pd.read_csv(
+        "users.dat",
+        sep="::",
+        engine="python",
+        names=["user_id", "gender", "age", "occupation", "zip"],
+        encoding="latin-1"
+    )
 
-#Convert all IDs into string so model cannot mistake it for number
-train["user_id"] = train["user_id"].astype(str)
-train["movie_id"] = train["movie_id"].astype(str)
+    #Drop empty and duplicates(training set only) dropping duplicates on val is not necessary
+    train = train.drop(columns=["timestamp"]).dropna().drop_duplicates()
+    val = val.drop(columns=["timestamp"]).dropna()
 
-val["user_id"] = val["user_id"].astype(str)
-val["movie_id"] = val["movie_id"].astype(str)
+    #Convert all IDs into string so model cannot mistake it for number
+    train["user_id"] = train["user_id"].astype(str)
+    train["movie_id"] = train["movie_id"].astype(str)
 
-movies["movie_id"] = movies["movie_id"].astype(str)
-users["user_id"] = users["user_id"].astype(str)
+    val["user_id"] = val["user_id"].astype(str)
+    val["movie_id"] = val["movie_id"].astype(str)
 
-#Grouping all movies that each user already watched into a dict eg. "1": {"1","2","3"} --> user 1 already seen movie 1,2,3
-#note that dict is not sorted numerically bc it is a string eg. {"1", "10", "100"}
-train_user_watched = train.groupby("user_id")["movie_id"].apply(set).to_dict()
+    movies["movie_id"] = movies["movie_id"].astype(str)
+    users["user_id"] = users["user_id"].astype(str)
 
-#creating dict for movies that should be recommended for faster search later "1":{"10"} --> should recommended movie 10 to user 1
-val_user_movie = val.set_index("user_id")["movie_id"].to_dict()
+    return train, val, movies, users
 
-#all of the movies
-all_movies = train["movie_id"].unique()
+def groupData(train, val):
+    #Grouping all movies that each user already watched into a dict eg. "1": {"1","2","3"} --> user 1 already seen movie 1,2,3
+    #note that dict is not sorted numerically bc it is a string eg. {"1", "10", "100"}
+    train_user_watched = train.groupby("user_id")["movie_id"].apply(set).to_dict()
+    
+    #creating dict for validation eg. "1":{"10"} --> if our model recommend movie 10 for user 1 it is "correct"
+    val_user_movie = val.set_index("user_id")["movie_id"].to_dict()
+    
+    #all of the movies
+    all_movies = train["movie_id"].unique()
+
+    return train_user_watched, val_user_movie, all_movies
 
 #Function to get all movies that the user has not seen yet
-def get_candidate_movies(user_id):
+def get_candidate_movies(user_id, train_user_watched, all_movies):
     seen = train_user_watched[user_id]
     return [m for m in all_movies if m not in seen]
+
+#Function to get all processed data
+def allData():
+    train, val, movies, users = cleanData()
+
+    train_user_watched, val_user_movie, all_movies = groupData(train, val)
+
+    return {
+        "train": train,  #cleaned training data
+        "val": val,  #validation data
+        "movies": movies,  #unfinished movies data
+        "users": users,  #cleaned user.dat
+        "train_user_watched": train_user_watched,  #dict of movies the user has already watched
+        "val_user_movie": val_user_movie,  # dict for precision calculation
+        "all_movies": all_movies #list of all unique movies from training set
+    }
+    
